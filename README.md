4531f83489dbc99dea442a684f390414b5c44b48
miketham24 <michaelthamk@gmail.com>
2025-01-21 13:05:49 -0800

    talk about switch -c with a new file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dsci-310-branch-conflict
+
+#git switch -c <name>: will create a new branch and switch to it
+echo "my second branch"
---
+++ script.sh
@@ -0,0 +1,6 @@
+#  this is a bash script file
+# run it like
+# bash script.sh
+
+#git switch -c <name>: will create a new branch and switch to it
+echo "my second branch"
